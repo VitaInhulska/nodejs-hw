7080507d7cc3be843085b23226f035d01596bdee
Vita Inhulska <gavrishenko.vita@gmail.com>
2025-10-08 17:14:34 +0300

    fixed totalnotes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -18,17 +18,17 @@ export const getAllNotes = async (req, res) => {
     ]);
   }
 
-  const [totaNotes, notes] = await Promise.all([
+  const [totalNotes, notes] = await Promise.all([
     notesQuery.clone().countDocuments(),
     notesQuery.skip(skip).limit(perPage),
   ]);
 
-  const totalPages = Math.ceil(totaNotes / perPage);
+  const totalPages = Math.ceil(totalNotes / perPage);
 
   res.status(200).json({
     page,
     perPage,
-    totaNotes,
+    totalNotes,
     totalPages,
     notes,
   });
