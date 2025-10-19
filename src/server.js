6691fa01f1ae17fe59a5644f701b75f84d094779
Vita Inhulska <gavrishenko.vita@gmail.com>
2025-10-19 16:01:25 +0300

    done avatar

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -24,6 +25,7 @@ app.use(helmet());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
